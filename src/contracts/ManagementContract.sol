3fad66133aa0a53836732e09c8d023337ed956a3
RomanFro <roman.frolov@wings.ai>
2018-03-27 15:08:47 +0300

    batteryFee fix

--- src/contracts/ManagementContract.sol
+++ src/contracts/ManagementContract.sol
@@ -2,45 +2,55 @@ pragma solidity ^0.4.19;
 
 import "./ManagementContractInterface.sol";
 import "./lib/Ownable.sol";
+import "./lib/SafeMath.sol";
 import "./BatteryManagement.sol";
 import "./ServiceProviderWallet.sol";
 import "./ERC20.sol";
 
 contract ManagementContract is Ownable {
+  using SafeMath for uint256;
+
   event Vendor(address, bytes4);
   event NewBattery(bytes4, bytes20);
 
-  uint256 public fee;
+  struct vendor {
+    bytes4 id;
+    uint256 deposit;
+  }
+
+  uint256 public batteryFee;
   ServiceProviderWallet public serviceProviderWallet;
   BatteryManagement public batteryManagement;
 
-  mapping (address => bytes4) vendors;
+  mapping (address => vendor) vendors;
 
-  function ManagementContract(address _serviceProviderWallet, uint256 _fee) {
+  function ManagementContract(address _serviceProviderWallet, uint256 _batteryFee) {
     serviceProviderWallet = ServiceProviderWallet(_serviceProviderWallet);
-    fee = _fee;
+    batteryFee = _batteryFee;
   }
 
   function setBatteryManagementContract(address _batteryManagement) public onlyOwner {
     batteryManagement = BatteryManagement(_batteryManagement);
   }
 
-  function setFee(uint256 _fee) public onlyOwner {
-    fee = _fee;
+  function setFee(uint256 _batteryFee) public onlyOwner {
+    batteryFee = _batteryFee;
   }
 
   function registerVendor(bytes _bytes) public payable {
-    require(msg.value >= fee);
+    require(msg.value >= batteryFee.mul(1000));
     bytes4 _bytes4 = bytes4(keccak256(msg.sender, _bytes, block.number));
-    vendors[msg.sender] = _bytes4;
+    vendors[msg.sender].id = _bytes4;
     Vendor(msg.sender, _bytes4);
   }
 
   function registerBatteries(bytes20[] ids) public payable {
-    require(vendors[msg.sender] != "");
-    for (uint256 i = 0; i < ids.length; i++) {
+    require(vendors[msg.sender].id != "");
+    uint256 amount = ids.length;
+    require(vendors[msg.sender].deposit >= batteryFee.mul(amount));
+    for (uint256 i = 0; i < amount; i++) {
       batteryManagement.createBattery(msg.sender, ids[i]);
-      NewBattery(vendors[msg.sender], ids[i]);
+      NewBattery(vendors[msg.sender].id, ids[i]);
     }
   }
 
