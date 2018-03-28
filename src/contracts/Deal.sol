6a1c876766d34f1bfdfcc6477d55b8eb582b4603
RomanFro <roman.frolov@wings.ai>
2018-03-28 16:20:05 +0300

    fix

--- src/contracts/Deal.sol
+++ src/contracts/Deal.sol
@@ -4,4 +4,15 @@ import "./DealInterface.sol";
 
 contract Deal {
 
+  function Deal(
+    bytes20 _idO,
+    bytes20 _idN,
+    address _token,
+    uint256 _amountCompensation,
+    uint256 _amountForWork,
+    uint256 _stubTime
+    ) {
+
+    }
+
 }
